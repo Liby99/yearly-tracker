89e5c647eae5bb0ab08ba7f3001a2207392ba896
Ziyang Li <liby99@icloud.com>
2025-07-09 15:32:44 -0400

    Adding more pointers to help and settings

--- src/app/components/YearlyTracker.tsx
+++ src/app/components/YearlyTracker.tsx
@@ -87,7 +87,11 @@ export default function YearlyTracker() {
         externalCalendar={configuration.externalCalendar}
       />
       <footer className="screen-only">
-        &copy; 2025 Liby99, all rights reserved.
+        &copy; 2025 <a href="https://liby99.github.io">Liby99</a>, all rights reserved.
+        <span style={{ display: "inline-block", margin: "0 5px" }}>|</span>
+        <a onClick={() => setShowHelp(true)}>Help</a>
+        <span style={{ display: "inline-block", margin: "0 5px" }}>|</span>
+        <a onClick={() => setShowSettings(true)}>Settings</a>
       </footer>
       
       <Configuration
